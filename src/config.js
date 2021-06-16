d74e966b293fab2bfff58a180e6306872daa7b16
Allison Schulman <allison.d.schulman@gmail.com>
2021-06-16 19:09:08 -0400

    Changed endpoint to heroku

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 const obj = {
-    API_ENDPOINT: `http://localhost:8000`,
-    // API_ENDPOINT: `https://mighty-reef-98621.herokuapp.com/`,
+    // API_ENDPOINT: `http://localhost:8000`,
+    API_ENDPOINT: `https://mighty-reef-98621.herokuapp.com/`,
 }
 
 export default obj;
