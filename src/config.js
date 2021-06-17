9818fa7bef42678c35caaa85849a2d7957db0f47
Allison Schulman <allison.d.schulman@gmail.com>
2021-06-17 09:42:07 -0400

    Removed extra / from API_BASE_URL

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 const config = {
     PORT: process.env.PORT || 8080,
     // API_BASE_URL: `http://localhost:8000`,
-    API_BASE_URL: process.env.REACT_APP_API_BASE_URL || `https://mighty-reef-98621.herokuapp.com/`,
+    API_BASE_URL: process.env.REACT_APP_API_BASE_URL || `https://mighty-reef-98621.herokuapp.com`,
 }
 
 export default config;
