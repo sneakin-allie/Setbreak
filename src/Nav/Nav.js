498114a854c91f0ea67a995b383f6cb1825a453e
Allison Schulman <allison.d.schulman@gmail.com>
2021-06-24 15:39:11 -0400

    Added log out and redirect

--- src/Nav/Nav.js
+++ src/Nav/Nav.js
@@ -15,6 +15,9 @@ class Nav extends React.Component {
                 <Link to="/add">
                     Add New Concert
                 </Link>
+                <Link to='/'>
+                    Log Out
+                </Link>
             </div>
         )
     }
