c567815d9f94bcab6bfb16e8ac26102bbbe4735f
Allison Schulman (she/her) <68669789+sneakin-allie@users.noreply.github.com>
2021-07-20 11:43:57 -0400

    Header image and responsiveness set

--- src/LandingPage/LandingPage.js
+++ src/LandingPage/LandingPage.js
@@ -8,11 +8,11 @@ class LandingPage extends React.Component {
         return (
             <div className="Landing-page">
                 <h3>Keep track of concerts you've attended</h3>
-                    <p className="Landing-page-p">Setbreak helps you keep track of concerts you've attended in one streamlined collection. Upload information about dates, artists, venues, highlights, and notes for each concert. All of your concerts in one collection!</p>
+                    <p className="Landing-page-p">Setbreak helps you keep track of concerts you've attended in one streamlined collection. Upload information about the artist, venue, date, highlights, and notes for each concert. All of your concerts in one collection!</p>
                 <h3>Keep notes on memorable moments</h3>
-                    <p className="Landing-page-p">Setbreak helps you remember the moments you never want to forget. Did the artist play your favorite song, make a funny joke, tell a story, or get creative with props? Keep notes of these moments and more!</p>
+                    <p className="Landing-page-p">Setbreak helps you remember the moments you never want to forget. Did the artist play your favorite song, make a funny joke, tell a story, or get creative with props? Keep notes on these moments and more!</p>
                 <h3>Add new concerts and edit as you go</h3>
-                    <p className="Landing-page-p">Setbreak allows you to add new concerts and edit and delete them as needed. Upload information about a concert halfway through the show, and then return to it later to add more details!</p>
+                    <p className="Landing-page-p">Setbreak allows you to add a new concert and edit and delete as needed. Upload information about a concert during setbreak and then return to it later to add more details!</p>
                 <div className="group">
                     <div className="item-double">
                         <SignUpForm onSignUp={this.props.onSignUp}/>
